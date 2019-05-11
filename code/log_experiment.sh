580c262dff8cac0eadac222d98b34b1a236f1aeb
Benardi <benardinunes@gmail.com>
2019-05-11 18:34:09 -0300

    Replace binary used to log time. Add perfomance experiment results
    Replace bash built-in time binary for /usr/bin/time in bash script.
    Add perfomance experiment results.

--- code/log_experiment.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-for i in {1..10} ; do
-    echo "program_name:$1"
-    echo "time_execution:$(date +%Y-%m-%d-%H:%M:%S)"
-    time $1 &>/dev/null
-    echo "" 
-done 2>&1 | tee --append timing.log
