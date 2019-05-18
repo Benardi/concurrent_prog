f8ad26cc1e741ec139588f4ddf2eea03f37697e2
Gustavo Moneiro <dinizmonteirogustavo@gmail.com>
2019-05-18 17:12:02 -0300

    Add method to test Map based collections.

--- java-code/src/ex_05/Main.java
+++ java-code/src/ex_05/Main.java
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Main {
     public final static int THREAD_POOL_SIZE = 10;
-	public final static int ENTRIES_SIZE = 1000;
+	public final static int ENTRIES_SIZE = 10000;
 
 
     public static Map<String, Integer> concurrentHashMapObject = null;
@@ -26,18 +26,20 @@ public class Main {
     public static List<Integer> synchronizedListObject = null;
     
     public static void main(String[] args) throws InterruptedException {
-		concurrentHashMapObject = new ConcurrentHashMap<String, Integer>();
-        synchronizedMapObject = Collections.synchronizedMap(new HashMap<String, Integer>());
-
-
         copyOnWriteListObject = new CopyOnWriteArrayList<>();
         testList(copyOnWriteListObject);
 
         synchronizedListObject = Collections.synchronizedList(new ArrayList<Integer>());
         testList(synchronizedListObject);
+
+
+        concurrentHashMapObject = new ConcurrentHashMap<String, Integer>();
+        testMap(concurrentHashMapObject);
+        synchronizedMapObject = Collections.synchronizedMap(new HashMap<String, Integer>());
+        testMap(synchronizedMapObject);
     }
     
-    public static void testList(final List<Integer> testSet) throws InterruptedException {
+    public static void testList(final List<Integer> testCollection) throws InterruptedException {
 		for (int i = 0; i < THREAD_POOL_SIZE; i++) {
 
 			long initialTime = System.nanoTime();
@@ -50,8 +52,8 @@ public class Main {
 						for (int i = 0; i < ENTRIES_SIZE; i++) {
 							Integer random = (int) Math.ceil(Math.random() * 550000);
 
-							testSet.add(i, random);
-							Integer notImportantNumber = testSet.get(i);
+							testCollection.add(i, random);
+							Integer notImportantNumber = testCollection.get(i);
 						}
 					}
 				});
@@ -62,6 +64,40 @@ public class Main {
 
 			long finalTime = System.nanoTime();
 			long totalTime = (finalTime - initialTime) / 1000000L;
+			System.out.println(totalTime);
+		}
+    }
+    
+    public static void testMap(final Map<String, Integer> testCollection) throws InterruptedException {
+		long averageTime = 0;
+		for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+
+			long initialTime = System.nanoTime();
+
+			ExecutorService executorServer = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
+			for (int j = 0; j < THREAD_POOL_SIZE; j++) {
+				executorServer.execute(new Runnable() {
+					@Override
+					public void run() {
+
+						for (int i = 0; i < ENTRIES_SIZE; i++) {
+                            Integer random = (int) Math.ceil(Math.random() * 550000);
+                            
+							testCollection.put(String.valueOf(random), random);
+							Integer notImportantNumber = testCollection.get(String.valueOf(random));
+						}
+					}
+				});
+			}
+
+			executorServer.shutdown();
+			executorServer.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+
+			long finalTime = System.nanoTime();
+			long totalTime = (finalTime - initialTime) / 1000000L;
+			averageTime += totalTime;
+
 			System.out.println(totalTime);
 		}
 	}
