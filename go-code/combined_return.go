835717d3a4cc1a27c5d0162727066d5bb3ba1f72
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-06-22 15:56:28 -0300

    refact go-q2

--- go-code/combined_return.go
+++ go-code/combined_return.go
@@ -5,40 +5,34 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 )
 
-type firstReturn struct {
-	sync.Mutex
-	value int
-}
-
-func request(id int, fstReturn *firstReturn, wg *sync.WaitGroup) {
+func request(id int, results chan int) {
 	sleepTime := 1 + rand.Intn(30)
 	fmt.Printf("goroutine %d: sleep %ds\n", id, sleepTime)
 	t := time.Duration(sleepTime) * time.Second
 	time.Sleep(t)
 
-	fstReturn.Lock()
-	fstReturn.value += sleepTime
-	fstReturn.Unlock()
-	wg.Done()
+	results <- sleepTime
 }
 
 func gateway(numReplicas int) int {
-	fstReturn := &firstReturn{value: 0}
-
-	var wg sync.WaitGroup
-	wg.Add(numReplicas)
+	results := make(chan int, numReplicas)
 
 	for i := 1; i <= numReplicas; i++ {
-		go request(i, fstReturn, &wg)
+		go request(i, results)
 	}
 
-	wg.Wait()
+	combinedResults := 0
+	for i := 1; i <= numReplicas; i++ {
+		select {
+		case currentResult := <-results:
+			combinedResults += currentResult
+		}
+	}
 
-	return fstReturn.value
+	return combinedResults
 }
 
 func main() {
