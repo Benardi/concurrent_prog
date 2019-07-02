92cfff1a78090a020c6b0cae1ecc3f0410d1f1df
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-07-02 15:09:28 -0300

    refact q2

--- java-code/src/list-1/ex_02/Messenger.java
+++ java-code/src/list-1/ex_02/Messenger.java
@@ -2,46 +2,57 @@ package ex_02;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.concurrent.Semaphore;
 
 class Messenger implements Channel {
 
-    private Queue<String> messages;
-    private Semaphore isFull;
-    private Semaphore isEmpty;
+    private final Queue<String> messages;
+    private int capacity;
 
     public Messenger(int capacity) {
         messages = new ArrayDeque<>();
-        isFull = new Semaphore(capacity);
-        isEmpty = new Semaphore(0);
+        this.capacity = capacity;
+    }
+
+    private boolean isEmpty() {
+        return messages.size() == 0;
+    }
+
+    private boolean isFull() {
+        return  messages.size() == capacity;
     }
 
     @Override
     public void putMessage(String message) {
-            try {
-                isFull.acquire();
-                synchronized (this) {
-                    messages.add(message);
-                    System.out.println("Put: " + message);
+        synchronized (messages) {
+            while(isFull()) {
+                try {
+                    messages.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                isEmpty.release();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
+            messages.add(message);
+            System.out.println("Put: " + message);
+            messages.notifyAll();
+        }
     }
 
     @Override
     public String takeMessage() {
-        String message = null;
+        synchronized (messages) {
+            String message = null;
             try {
-                isEmpty.acquire();
-                synchronized (this) {
-                    message = messages.remove();
+                while(isEmpty()) {
+                    this.messages.wait();
                 }
-                isFull.release();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        return message;
+            message = messages.remove();
+            System.out.println("Take: " + message);
+            this.messages.notifyAll();
+            return message;
+        }
+
     }
 }
